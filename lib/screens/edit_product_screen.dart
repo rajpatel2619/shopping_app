8a8ad534456bdc4645c1aec2667e18149e61e927
raj patel <kakhilesh79@gmail.com>
2020-09-25 11:38:19 +0530

    changes

---
+++ lib/screens/edit_product_screen.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+class EditProductScreen extends StatefulWidget {
+  static const routeName = '/edit-product';
+  @override
+  _EditProductScreenState createState() => _EditProductScreenState();
+}
+
+class _EditProductScreenState extends State<EditProductScreen> {
+  final _priceFocusNode = FocusNode();
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text('Edit Product'),
+        ),
+        body: Padding(
+          padding: EdgeInsets.all(16),
+          child: Form(
+              child: ListView(
+            children: [
+              TextFormField(
+                decoration: InputDecoration(labelText: 'Title'),
+                textInputAction: TextInputAction.next,
+              ),
+              TextFormField(
+                decoration: InputDecoration(labelText: 'Price'),
+                textInputAction: TextInputAction.next,
+                keyboardType: TextInputType.number,
+              ),
+            ],
+          )),
+        ));
+  }
+}
--- lib/widgets/cart_item.dart
+++ lib/widgets/cart_item.dart
@@ -9,14 +9,35 @@ class CartItem extends StatelessWidget {
   final int quantity;
   final String title;
 
-  CartItem(this.id,this.productId, this.price, this.quantity, this.title);
+  CartItem(this.id, this.productId, this.price, this.quantity, this.title);
 
   @override
   Widget build(BuildContext context) {
     return Dismissible(
       direction: DismissDirection.endToStart,
+      confirmDismiss: (direction) {
+        // return Future.value(true);
+        return showDialog(
+          context: context,
+          builder: (ctx) => AlertDialog(
+            title: Text('Are you sure?'),
+            content: Text('Do you want to remove this item from the cart?'),
+            actions: [
+              FlatButton(
+                  onPressed: () {
+                    Navigator.of(ctx).pop(false);
+                  },
+                  child: Text('No')),
+              FlatButton(onPressed: () {
+                    Navigator.of(ctx).pop(true);
+
+              }, child: Text('Yes')),
+            ],
+          ),
+        );
+      },
       onDismissed: (direction) {
-        Provider.of<Cart>(context,listen: false).removeItem(productId);
+        Provider.of<Cart>(context, listen: false).removeItem(productId);
       },
       key: ValueKey(id),
       background: Container(
